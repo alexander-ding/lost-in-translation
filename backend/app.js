3c14d1371c4f3bf3de4393fc569bb7819ef23648
Alexander Ding <alexander.jf.ding@gmail.com>
2021-06-20 22:06:49 -0400

    Added cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const apiRouter = require('./api');
 const port = process.env.PORT || 4000;
 
+app.use(cors({
+  origin: 'https://alexander-ding.github.io'
+}));
 app.use(express.json());
 app.use('/api', apiRouter);
 
